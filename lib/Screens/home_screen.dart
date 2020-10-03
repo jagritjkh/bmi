618a1a489ded356f140c0007a6cd77d16e9fe1a5
Harshleen Kaur <h.kaur.makkar16@gmail.com>
2020-10-03 18:28:17 +0530

    Update home_screen.dart

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -20,9 +20,11 @@ class _HomeScreenState extends State<HomeScreen> {
             ));
   }
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.center,
